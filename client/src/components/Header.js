9d25acacec5089fffcb7cf7941c9f32c6eb3ea44
Zachary Ruffner <locutus1986@gmail.com>
2021-05-31 17:31:39 -0400

    header complete

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div> 
-            <h1>Header</h1>
+        <div className="ui secondary pointing menu"> 
+            <Link to='/' className='item'>Streamy</Link>
+            <div className="right menu">
+                <Link to='/' className='item'> All Streams</Link>
+            </div>
         </div>
     )
 }
