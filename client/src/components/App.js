e97611a445bdedbe96aa114b7cdea07bcff8e863
Zachary Ruffner <locutus1986@gmail.com>
2021-05-31 17:14:11 -0400

    routs complete

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-const pageOne = () => {
-    return <div> pageOne 
-        <Link to='/pageTwo'> Navigate to pageTwo </Link>
-    </div>
-}
+import Header from './Header'
+import StreamCreate from './streams/StreamCreate'
+import StreamDelete from './streams/StreamDelete'
+import StreamEdit from './streams/StreamEdit'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
 
-const pageTwo = () => {
-    return <div>pageTwo 
-        <Link to='/'> Navigate to pageOne </Link>
-    </div>
-}
 
 const App = () => {
     return <div>
+        <Header />
         <BrowserRouter>
             <div>
-                <Route path='/' exact component={pageOne} />
-                <Route path='/pageTwo' component={pageTwo} />
+                <Route path='/' exact component={StreamList} />
+                <Route path='/streams/new' exact component={StreamCreate} />
+                <Route path='/streams/edit' exact component={StreamEdit} />
+                <Route path='/streams/delete' exact component={StreamDelete} />
+                <Route path='/streams/show' exact component={StreamShow} />
             </div>
         </BrowserRouter>     
     </div>
